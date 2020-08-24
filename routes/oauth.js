c636a9a262b6205fd92bbc74fa5d25e819c3ab96
Pisut Sritrakulchai <pudkrong@hotmail.com>
2020-08-24 13:29:28 +0700

    feat(Shared session):

--- routes/oauth.js
+++ routes/oauth.js
@@ -21,9 +21,11 @@ router.get('/eko', async function(req, res, next) {
       const state = crypto.randomBytes(16).toString('hex');
       
       // Store state into session
-      const stateMap = req.session.stateMap || {};      
-      stateMap[state] = req.query.redirectTo;
-      req.session.stateMap = stateMap;
+      // const stateMap = req.session.stateMap || {};      
+      // stateMap[state] = req.query.redirectTo;
+      // req.session.stateMap = stateMap;
+      req.session.state = state;
+      req.session.redirectTo = req.query.redirectTo;
 
       const uri = clientOauth.code.getUri({ state });
       res.redirect(uri);
@@ -39,16 +41,18 @@ router.get('/eko', async function(req, res, next) {
 router.get('/eko/callback', async function(req, res, next) {
   try {
     // Make sure it is the callback from what we have initiated
-    const state = req.query.state || '';
+    const state = req.session.state;
+    const redirectTo = req.session.redirectTo;
+
     const { client, data } = await clientOauth.code.getToken(req.originalUrl, { state });
     const user = JWT.decode(data.id_token);
     req.session.user = user;
 
     // Get redirectTo from state
-    const stateMap = req.session.stateMap || {};
-    const redirectTo = stateMap[state] || 'https://sso.pud.local/error';
-    delete stateMap[state];
-    req.session.stateMap = stateMap;
+    // const stateMap = req.session.stateMap || {};
+    // const redirectTo = stateMap[state] || 'https://sso.pud.local/error';
+    // delete stateMap[state];
+    // req.session.stateMap = stateMap;
 
     res.redirect(redirectTo);
   } catch (error) {
